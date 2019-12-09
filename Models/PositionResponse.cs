9214b15c2cfc63ce5b654b5f0bd4ff97164216a4
Zak <34078361+BoSkeet@users.noreply.github.com>
2019-12-09 21:25:40 +0000

    Beta API
    first iteration of backend api for devbot position functionality

---
+++ Models/PositionResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartelBotAPI.Models
+{
+    public class PositionResponse
+    {
+        public long position { get; set; }
+        public float entry { get; set; }
+        public string name { get; set; }
+    }
+}
