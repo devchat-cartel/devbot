5ce98086f55db805f9c84cf3ef7fcc4c1d28c112
Congrav1ty <asdf@asdf.com>
2019-12-18 15:56:02 -0500

    Updated packages in requirements.txt to latest versions; changed python from 3.5 to latest; updated bot.py code to work with discord.py v1.0 and above. CODE STILL DOESN'T WORK QUITE YET need to resolve ext (extensions) library error

--- bot.py
+++ bot.py
@@ -9,6 +9,11 @@ GITHUB_CHECK_INTERVAL = 60
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
 client = discord.Client()
+bot = discord.ext.commands.Bot()
+
+# the #general channel
+general = bot.get_channel(518770364042444850)
+
 
 def get_last_github_push():
     response = requests.get(
@@ -22,60 +27,58 @@ def get_last_github_push():
     except:
         return 'Unknown'
 
+
 async def background_task_github_push():
-    await client.wait_until_ready()
+    await bot.wait_until_ready()
     latest_push = None
-    while not client.is_closed:
+    while not bot.is_closed():
         try:
             last_push = get_last_github_push()
-            if latest_push != None and last_push > latest_push:
+            if latest_push and last_push > latest_push:
                 latest_push = last_push
-                await client.send_message(
-                    client.get_channel('518770364042444850'),
-                    'New commit found at {latest_push} !'.format(
-                        latest_push=get_last_github_push()
-                    )
-                )
+                await general.send(f'New commit found at {latest_push} !')
             else:
                 await asyncio.sleep(GITHUB_CHECK_INTERVAL)
         except Exception as e:
             print(str(e))
             await asyncio.sleep(GITHUB_CHECK_INTERVAL)
 
+
 @client.event
 async def on_ready():
     print('Ready')
 
+
 @client.event
 async def on_message(message):
-    if message.author != client.user:
-        print(
-            ' | '.join(
-                str(e)
-                for e in (
-                    message.server,
-                    message.channel,
-                    message.author,
-                    message.content
-                )
+    if message.author == client.user:
+        return
+    # if message.author != client.user:
+    print(
+        ' | '.join(
+            str(e)
+            for e in (
+                message.guild,
+                message.channel,
+                message.author,
+                message.content
             )
         )
+    )
 
-        if message.content.startswith('. '):
-            if message.content[2:].startswith('last'):
-                await client.send_message(
-                    message.channel,
-                    'Last commit was at {last_push}'.format(
-                        last_push=get_last_github_push()
-                    )
+    if message.content.startswith('. '):
+        if message.content[2:].startswith('last'):
+            await general.send(
+                'Last commit was at {last_push}'.format(
+                    last_push=get_last_github_push()
                 )
-            else:
-                await client.send_message(
-                    message.channel,
-                    'Unknown command: {message}'.format(
-                        message=message.content[2:]
-                    )
+            )
+        else:
+            await general.send(
+                'Unknown command: {message}'.format(
+                    message=message.content[2:]
                 )
+            )
 
 if __name__ == '__main__':
     client.loop.create_task(
