cec344cb1b455cc493a869d73cf856e984b3c222
Congrav1ty <58407968+Congrav1ty@users.noreply.github.com>
2020-01-06 16:12:55 -0500

    Created ". help" command (DM's the help menu to the user that called it)

--- bot.py
+++ bot.py
@@ -27,11 +27,13 @@ def get_last_github_push():
 
 
 @bot.command(name='private')
+@commands.cooldown(1, 10, commands.BucketType.user)
 async def _priv8(ctx):
     await ctx.send('Found me !')
 
 
 @bot.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
 async def last_commit(ctx):
     await ctx.send(
         'Last commit was at {last_push}'.format(
@@ -41,6 +43,7 @@ async def last_commit(ctx):
 
 
 @bot.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
 async def echo(ctx, *, message):
     await ctx.send(message)
 
@@ -62,12 +65,45 @@ async def background_task_github_push():
             await asyncio.sleep(GITHUB_CHECK_INTERVAL)
 
 
+@commands.command()
+async def help(ctx):
+    dmchannel = ctx.author.create_dm()
+    await dmchannel.send(f"Position tracker help :)))))"
+                   f"\n"
+                   f"\nCOMMANDS"
+                   f"\n    . help               | displays positiontracker's help menu   "
+                   f"\n    . position           | shows your current /position in a public channel (# contracts, long/short, entry)"
+                   f"\n    . echo <message>     | echoes back your message in the channel"
+                   f"\n    . api <key> <secret> | (DM-only) sets up your Bitmex API credentials"
+                   f"\n    . remove             | (DM-only) removes your Bitmex API credentials from the bot"
+                   f"\n"
+                   f"\nAPI KEY SETUP"
+                   f"\n    To retrieve your current /position, you need to tell the bot your Bitmex API keys."
+                   f"\n    In your bitmex account settings, create a READ-ONLY API key (do not select 'order'"
+                   f"\n    or 'order cancel' in the drop-down; just leave it on the default '-'."
+                   f"\n    "
+                   f"\n    Then, DM me (the bot) using the command:"
+                   f"\n    `. api <key> <secret>`"
+                   f"\n    for example:"
+                   f"\n    `. api . api nBRcCH6uE49KgLCMjJn09DEA -jNDyaMgwqoZT8V3-4Hmx5oA5UeNOYICNk3dl4H6w1-s5sxA"
+                   f"\n    "
+                   f"\n    If you want the bot to forget your API keys, just DM me the '. remove' command instead:"
+                   f"\n    `. remove`"
+                   f"\n    "
+                   f"\nSHOWING YOUR POSITION"
+                   f"\n    In a public channel (like #bitmex), simply type the '. position' command:"
+                   f"\n    `. position`"
+                   f"\n    And your current position will be displayed, with the # of contracts,"
+                   f"\n    long or short, and your entry price."
+
+
 @bot.event
 async def on_ready():
     print('Ready')
     guilds = await bot.fetch_guilds(limit=5).flatten()     # guilds is now a list of Guild...
     for guild in guilds:
         print(guild)
+
 
 @bot.event
 async def on_message(message):
